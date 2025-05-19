bd2ecab44389f93f9eea58e77c9951cabe26cb61
Song <songzhang015@gmail.com>
2025-05-19 11:33:25 -0700

    Backend homepage, index, and login

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,7 @@ Used for storing data between app and MongoDB
 Currently not connected to mobile app
 """
 
-from flask import Flask
+from flask import Flask, redirect, url_for
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -19,8 +19,17 @@ client = MongoClient('localhost', 27017)
 db = client.flask_database
 
 @app.route('/')
-def hello_world():
-    return "Hello, World!"
+def index():
+    """Redirects user to the home page."""
+    return redirect(url_for("home"))
+
+@app.route('/home')
+def home():
+    return "Home"
+
+@app.route('/login')
+def login():
+    return "Login"
 
 if __name__ == '__main__':
     app.run(debug=True)
