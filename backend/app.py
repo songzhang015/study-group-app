8381a55d29eda361b35bc0230ac982680699230f
SlowTsuki <derekv@uoregon.edu>
2025-05-23 14:44:53 -0700

    Worked on db_models.py, class StudyGroup(Document)

--- backend/app.py
+++ backend/app.py
@@ -13,6 +13,7 @@ Currently not connected to mobile app
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
 
+
 app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)
