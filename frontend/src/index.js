6d15704118f33437db6770cc4d065fb807de8b7f
dkmk100 <33072952+dkmk100@users.noreply.github.com>
2025-05-27 12:26:28 -0700

    begin creating login screen
    Added a skeleton for the login screen, though it doesn't perform navigation yet

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './home';
+import Login from './login';
 import './home.css';
+import './login.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Login />
   </React.StrictMode>
 );
---
+++ frontend/src/login.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import './login.css';
+
+export default function Login() {
+    const [user, setUser] : [string, React.Dispatch<React.SetStateAction<string>>] = useState("");
+    const defaultUsers = ["user1", "user2", "user3"];
+
+    return (
+    <div className="container">
+        <div className="usernameInputContainer">
+        <Options setSelected={setUser} categories={defaultUsers}/>
+        </div>
+
+        <div className="loginButtonContainer">
+            <div className="text">user: {user}</div>
+            <button className="button">Login</button>
+        </div>
+    </div>
+    );
+}
+
+//TODO: create some kind of dropdown menu for this
+function Options({ setSelected, categories } : {
+    setSelected : React.Dispatch<React.SetStateAction<string>>,
+    categories : string[]
+}){
+    return (
+        //for each category, make a button that sets the selected one to that category
+        //and just display the categories
+        <menu>
+            {categories.map(category => (
+                <button key={category} onClick={() => setSelected(category)}>
+                    {category}
+                </button>
+            ))}            
+        </menu>
+    )
+}
